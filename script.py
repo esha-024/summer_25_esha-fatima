f8ff069766ebf2ca5e849f26dd094f668e8e2162
esha-024 <eshafatima640@gmail.com>
2025-06-24 11:34:13 +0500

    the first tarining of python

---
+++ script.py
@@ -0,0 +1,20 @@
+
+dna =1
+rna=2
+
+print(dna+rna)
+
+seq1= "ATAGCTTTTGCTAAAGTAAGC"
+SEQ2="GCGCG"
+seq3="T"
+print(seq1==SEQ2)
+print(seq1!=SEQ2)
+print(seq1<SEQ2)
+print(seq1>SEQ2) 
+
+
+
+
+import sys 
+ 
+
